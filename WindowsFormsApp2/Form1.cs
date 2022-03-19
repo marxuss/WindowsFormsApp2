6ad117d31e3241a5b142c37d4a73eaac68dbba84
marxuss <martins.banis@gmail.com>
2022-03-19 09:31:36 +0200

    Hello world

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -16,5 +16,20 @@ namespace WindowsFormsApp2
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Hello you";
+        }
     }
 }
